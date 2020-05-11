703276e3ef2c04714770c25662da54319ac7a91d
Krzysztof Talar <krzysztoftalar@protonmail.com>
2020-05-11 17:05:56 +0200

    Final touches

---
+++ src/WebUI/Configurations/JwtIdentityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebUI.Configurations
+{
+    public static class JwtIdentityConfiguration
+    {
+        public static IServiceCollection AddJwtIdentity(this IServiceCollection services, IConfigurationSection jwtConfiguration)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration["SecurityKey"]));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKey = key,
+                      ValidateAudience = false,
+                      ValidateIssuer = false
+                  };
+              });
+
+            return services;
+        }
+    }
+}
